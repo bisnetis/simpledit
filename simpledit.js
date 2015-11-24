27f9e89d04580d4c3d759bede1a0a30e57a85313
Rosscotini <rosscotini@gmail.com>
2015-11-24 09:48:23 +0200

    Update simpledit.js

--- simpledit.js
+++ simpledit.js
@@ -52,27 +52,27 @@ $.fn.simplEdit = function() {
             return text;
         }
         function getHTMLOfSelection () {
-          var range;
-          if (document.selection && document.selection.createRange) {
-            range = document.selection.createRange();
-            return range.htmlText;
-          }
-          else if (window.getSelection) {
-            var selection = window.getSelection();
-            if (selection.rangeCount > 0) {
-              range = selection.getRangeAt(0);
-              var clonedSelection = range.cloneContents();
-              var div = document.createElement('div');
-              div.appendChild(clonedSelection);
-              return div.innerHTML;
+            var range;
+            if (document.selection && document.selection.createRange) {
+                range = document.selection.createRange();
+                return range.htmlText;
+            }
+            else if (window.getSelection) {
+                var selection = window.getSelection();
+                if (selection.rangeCount > 0) {
+                    range = selection.getRangeAt(0);
+                    var clonedSelection = range.cloneContents();
+                    var div = document.createElement('div');
+                    div.appendChild(clonedSelection);
+                    return div.innerHTML;
+                }
+                else {
+                    return '';
+                }
             }
             else {
-              return '';
+                return '';
             }
-          }
-          else {
-            return '';
-          }
         }
         
         function processPaste (context, content) {
